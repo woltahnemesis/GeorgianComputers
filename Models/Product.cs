50582c132b678699fc8a32b085ac7e293230a6c1
Rey Walter Magbanua <alucardmagbanua@gmail.com>
2020-06-02 00:16:36 -0400

    Created a mock model of a product until I get the Dbase set up next time!

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Georgian_Computers.Models
+{
+    public class Product
+    {
+        //Read and property
+        public string Name { get; set; }
+    }
+}
