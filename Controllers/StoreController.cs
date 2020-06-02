de4f9f4d9faf7710f85ae54832ba435c6d6c405b
Rey Walter Magbanua <alucardmagbanua@gmail.com>
2020-06-01 22:18:50 -0400

    Added a StoreController and a ViewBag of products.

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Georgian_Computers.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string product)
+        {
+            //ViewBag is automatically passed back to the view that loads
+            ViewBag.product = product;
+            return View();
+        }
+    }
+}
